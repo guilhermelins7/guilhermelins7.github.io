c3edb13faad3bd16c68767ee7a53d4b1c03ba6ee
guilhermelins7 <guilherme7lins@gmail.com>
2024-10-27 14:27:10 -0300

    primeira versão finalizada

---
+++ js/script.js
@@ -0,0 +1,18 @@
+// Tranformar menu hamburguer em X
+
+const menuHamburguer = document.querySelector('.menu-hamburguer');
+
+menuHamburguer.addEventListener('click', () => {
+    mudarMenu();
+});
+
+function mudarMenu() {
+    const nav = document.querySelector('.nav-responsive');
+    menuHamburguer.classList.toggle('change');
+
+    if (menuHamburguer.classList.contains('change')) {
+        nav.style.display = 'block';
+    } else {
+        nav.style.display = 'none';
+    }
+}
