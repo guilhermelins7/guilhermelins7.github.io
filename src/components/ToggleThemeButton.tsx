92ca6535ecc6a08c73b5f68a51002600312190e7
guilhermelins7 <guilherme7lins@gmail.com>
2026-05-02 02:24:11 -0300

    refactor(header): Componentizar botão para troca de tema

---
+++ src/components/ToggleThemeButton.tsx
@@ -0,0 +1,35 @@
+import { useEffect, useState } from "react";
+
+export default function ToggleThemeButton() {
+        const [theme, setTheme] = useState(() => {
+        if(typeof window != "undefined") {
+            const storedTheme = window.localStorage.getItem("theme");
+            if(storedTheme) return storedTheme;
+            return window.matchMedia("(prefers-color-scheme: dark)").matches ? "dark" : "light";
+        }
+        return "light"
+    });
+
+    useEffect(() => {
+        const root = window.document.documentElement;
+        root.classList.remove("light", "dark");
+        root.classList.add(theme);
+        window.localStorage.setItem("theme", theme);
+    }, [theme]);
+
+    const toggleTheme = () => {
+        setTheme((prevTheme) => (prevTheme === "light" ? "dark" : "light"));
+    }
+
+  return (
+    <button
+      className="p-2 rounded-full hover:bg-gray-100 dark:hover:bg-gray-800 transition-colors cursor-pointer"
+      id="theme-toggle"
+      onClick={toggleTheme}
+    >
+      <span className="material-icons text-sm">
+        {theme === "dark" ? "light_mode" : "dark_mode"}
+      </span>
+    </button>
+  );
+}
--- src/sections/Header.tsx
+++ src/sections/Header.tsx
@@ -1,5 +1,5 @@
-import { useEffect, useState } from "react";
 import NavItem from "../components/NavItem";
+import ToggleThemeButton from "../components/ToggleThemeButton";
 
 export default function Header() {
     const links = [
@@ -11,26 +11,6 @@ export default function Header() {
         { label: "Entre em contato", href: "#contato" },
     ];
 
-    const [theme, setTheme] = useState(() => {
-        if(typeof window != "undefined") {
-            const storedTheme = window.localStorage.getItem("theme");
-            if(storedTheme) return storedTheme;
-            return window.matchMedia("(prefers-color-scheme: dark)").matches ? "dark" : "light";
-        }
-        return "light"
-    });
-
-    useEffect(() => {
-        const root = window.document.documentElement;
-        root.classList.remove("light", "dark");
-        root.classList.add(theme);
-        window.localStorage.setItem("theme", theme);
-    }, [theme]);
-
-    const toggleTheme = () => {
-        setTheme((prevTheme) => (prevTheme === "light" ? "dark" : "light"));
-    }
-
   return (
     <header>
       <nav className="fixed top-6 left-1/2 transform -translate-x-1/2 z-50 bg-white/80 dark:bg-[#20131F]/80 backdrop-blur-md px-6 py-3 rounded-full border border-gray-200 dark:border-gray-700 shadow-lg flex items-center gap-8">
@@ -40,15 +20,7 @@ export default function Header() {
                 href={link.href}
             />
         ))}
-        <button
-            className="p-2 rounded-full hover:bg-gray-100 dark:hover:bg-gray-800 transition-colors"
-            id="theme-toggle"
-            onClick={toggleTheme}
-        >
-            <span className="material-icons text-sm">
-                {theme === 'dark' ? 'light_mode' : 'dark_mode'}
-            </span>
-        </button>
+        <ToggleThemeButton/>
       </nav>
     </header>
   );
